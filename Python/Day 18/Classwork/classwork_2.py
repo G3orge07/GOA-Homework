5bac7403a0235538a934ec530273f1b2476fdfac
G3orge07 <giorgi.tavadze0701@gmail.com>
2024-11-27 18:56:53 +0400

    11th commit

---
+++ Python/Day 18/Classwork/classwork_2.py
@@ -0,0 +1,7 @@
+
+num = int(input('Enter a number: '))
+
+if num % 5 == 0:
+    print(str(num) + ' is a multiple of 5')
+else:
+    print(str(num) + ' is not a multiple of 5')
---
+++ Python/Day 7/Classwork/classwork_1.py
@@ -0,0 +1,11 @@
+
+num1 = int(input('1st number: '))
+num2 = int(input('2nd number: '))
+
+print(str(num1) + ' + ' + str(num2) + ' = ' + str(num1+num2))
+
+print(str(num1) + ' - ' + str(num2) + ' = ' + str(num1-num2))
+
+print(str(num1) + ' * ' + str(num2) + ' = ' + str(num1*num2))
+
+print(str(num1) + ' / ' + str(num2) + ' = ' + str(int(num1/num2)))
