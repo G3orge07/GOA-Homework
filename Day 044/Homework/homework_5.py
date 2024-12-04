2212d39e359a03175dd5245107acdea9a41b4df8
G3orge07 <giorgi.tavadze0701@gmail.com>
2024-12-04 23:24:48 +0400

    15th commit

--- Day 044/Homework/homework_5.py
+++ Day 044/Homework/homework_5.py
@@ -1,1 +1,12 @@
 # Convert number to reversed array of digits
+
+def digitize(n):
+    digits = []
+    for digit in str(n):
+        digits.append(int(digit))
+    
+    digits_rvrs = []
+    for i in range(-1, -(len(digits)+1), -1):
+        digits_rvrs.append(digits[i])
+        
+    return digits_rvrs
